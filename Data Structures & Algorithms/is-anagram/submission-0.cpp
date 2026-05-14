f0426837e95ac2cde6286811aea366854637cae1
Md. Abdullah Himel <himel22205101622@diu.edu.bd>
2026-05-14 13:50:34 +0600

    Add: is-anagram - submission-0

---
+++ Data Structures & Algorithms/is-anagram/submission-0.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if(s.size() != t.size())
+            return false;
+
+        map<char,int> mp;
+
+        for(char c: s){
+            mp[c]++;
+        }
+        
+        for(char c: t){
+            mp[c]--;
+            if(mp[c]<0)
+                return false;
+        }
+
+        for(auto [x,y]:mp)
+            cout<<x<<": "<<y<<endl;
+
+        return true;
+    }
+};
