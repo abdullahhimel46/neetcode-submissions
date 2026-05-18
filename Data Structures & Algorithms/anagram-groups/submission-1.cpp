6637bcf83bba52b7df170a02fd44b61f646b465c
Md. Abdullah Himel <himel22205101622@diu.edu.bd>
2026-05-18 11:28:59 +0600

    Add: anagram-groups - submission-1

---
+++ Data Structures & Algorithms/anagram-groups/submission-1.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& str) {
+        int n = str.size();
+        if(n<2) return {str};
+       
+       unordered_map<string,vector<string>> mp;
+
+       for(string w: str){
+        string tmp = w;
+        sort(tmp.begin(),tmp.end());
+
+        mp[tmp].push_back(w);
+       }
+
+       vector<vector<string>> ans;
+
+       for(auto [x,y]:mp){
+        ans.push_back(y);
+       }
+
+       return ans;
+    }
+
+};
