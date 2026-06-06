206e8fe13033669718607b96253be5813f9abd92
Md. Abdullah Himel <himel22205101622@diu.edu.bd>
2026-06-06 12:11:31 +0600

    Add: minimum-window-with-characters - submission-3

---
+++ Data Structures & Algorithms/minimum-window-with-characters/submission-3.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    string minWindow(string s, string t) {
+        int ls = s.size();
+        int lt = t.size();
+
+        if(lt>ls) return "";
+
+        int i=0, j=0, sIdx=-1, minLen=INT_MAX, cnt=lt;
+        // unordered_map<char,int> freq;
+        // vector<int> freq(128,0);
+        int freq[128] = {};
+        for(char c:t)
+            freq[c]++;
+
+        while(j<ls){
+            // hanlding the ending part of s
+            char cj = s[j];
+            if(freq[cj]>0)
+                cnt--;
+            freq[cj]--;
+            j++;
+
+            while(cnt==0) { // valid window
+                if(j-i < minLen){
+                    minLen = j-i;
+                    sIdx = i;
+                }
+
+                // hanlding the starting part of s
+                char ci = s[i];
+                //shrinking
+                freq[ci]++;
+                if(freq[ci]>0)
+                    cnt++;
+                i++;
+            }
+        }
+
+        return (minLen == INT_MAX) ? "" : s.substr(sIdx,minLen);
+    }
+};
